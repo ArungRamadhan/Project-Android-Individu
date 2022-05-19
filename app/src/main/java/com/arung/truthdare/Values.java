53bf3559158e72dcb29a167d1daad8dac7f12718
ArungRamadhan <65582917+ArungRamadhan@users.noreply.github.com>
2022-05-19 21:47:31 +0700

    push

---
+++ app/src/main/java/com/arung/truthdare/Values.java
@@ -0,0 +1,25 @@
+package com.arung.truthdare;
+
+public class Values {
+
+    String[] truths = new String[]{
+            "Acara TV apa yang paling kamu benci? Beri tahu alasannya!",
+            "Kapan terakhir kali kamu berbohong?",
+            "Siapa orang yang diam-diam kamu sukai?",
+            "Kapan terakhir kali kamu kentut?",
+            "Apa hal yang membuatmu senang orangtuamu tidak mengetahuinya?",
+            "Apa hal terburuk yang pernah kamu lakukan?",
+            "Apa hal yang paling membuatmu merasa insecure?",
+            "Hal terburuk apa yang pernah kamu katakan/lakukan terhadap orang lain?"
+    };
+
+    String[] dares = new String[]{
+            "Lakukan rap gaya bebas selama 3 menit.",
+            "Cium salah satu kaus kaki di antara kita.",
+            "Ungkapkan perasaanmu kepada gebetanmu.",
+            "Push up 20 kali.",
+            "Makan satu gigitan kulit pisang.",
+            "tirukan suara anjing selama 10 detik",
+            "Biarkan orang lain membuat status menggunakan akun sosial mediamu."
+    };
+}
